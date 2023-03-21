b9c49a3bc2fd87b68da14f6bdd5502ef0f6a01b3
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-03-21 01:01:15 +0000

    Update photo_dao.dart

--- glum_mood_tracker/lib/infrastructure/database/daos/photo_dao.dart
+++ glum_mood_tracker/lib/infrastructure/database/daos/photo_dao.dart
@@ -7,14 +7,17 @@ part 'photo_dao.g.dart';
 
 @DriftAccessor(tables: [Photos])
 class PhotoDao extends DatabaseAccessor<GlumDatabase> with _$PhotoDaoMixin {
+  // Constructor
   PhotoDao(this.db) : super(db);
   final GlumDatabase db;
 
+  // Fetch all photos from the database
   Future<List<PhotoDto>> getAllPhotos() async {
     final photoData = await select(photos).get();
     return photoData.map((e) => PhotoDto.fromJson(e.toJson())).toList();
   }
 
+  // Insert a new photo record into the database
   Future<int> insertPhoto(PhotoDto photoDto) async {
     final photo = PhotosCompanion.insert(
       fileName: photoDto.fileName,
@@ -23,6 +26,7 @@ class PhotoDao extends DatabaseAccessor<GlumDatabase> with _$PhotoDaoMixin {
     return await into(photos).insert(photo);
   }
 
+  // Fetch a photo by its ID
   Future<PhotoDto?> getPhotoById(int id) async {
     final query = select(photos)..where((row) => row.id.equals(id));
     final photo = await query.getSingleOrNull();
