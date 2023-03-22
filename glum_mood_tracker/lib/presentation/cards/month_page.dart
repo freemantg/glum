ea8b6835f97f1e26fb13b2ee3eed6731c69deb76
Freeman Tang™ <35115094+freemantg@users.noreply.github.com>
2023-03-22 12:39:53 +0000

    Refactored MonthPage

--- glum_mood_tracker/lib/presentation/cards/month_page.dart
+++ glum_mood_tracker/lib/presentation/cards/month_page.dart
@@ -1,29 +1,25 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:glum_mood_tracker/shared/extensions.dart';
 import 'package:glum_mood_tracker/styles/styles.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../domain/models/models.dart';
 import '../routes/app_router.gr.dart';
 import '../../shared/providers.dart';
 import '../stats/widgets/styled_card.dart';
 import 'widgets/widgets.dart';
 
-class MonthPage extends ConsumerStatefulWidget {
+class MonthPage extends HookConsumerWidget {
   const MonthPage({
-    super.key,
+    Key? key,
     required this.monthYear,
-  });
+  }) : super(key: key);
 
   final DateTime monthYear;
 
   @override
-  ConsumerState<MonthPage> createState() => _MonthPageState();
-}
-
-class _MonthPageState extends ConsumerState<MonthPage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: _buildAppBar(),
       body: LayoutBuilder(
@@ -35,67 +31,14 @@ class _MonthPageState extends ConsumerState<MonthPage> {
                     itemCount: stories.length,
                     itemBuilder: (context, index) {
                       final story = stories[index];
-
-                      return GestureDetector(
+                      return _StoryItem(
+                        story: story,
+                        maxHeight: constraints.maxHeight / 4,
                         onLongPress: () => ref
                             .read(storyNotifierProvider.notifier)
                             .deleteStory(story.id!),
                         onTap: () =>
                             context.router.push(StoryPageRoute(story: story)),
-                        child: StyledCard(
-                          customPadding: true,
-                          child: ConstrainedBox(
-                            constraints: BoxConstraints(
-                              maxHeight: constraints.maxHeight / 4,
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.all($styles.insets.lg),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      SizedBox(height: $styles.insets.xs),
-                                      Text(
-                                        story.date.day.toString(),
-                                        style: $styles.text.h2Condensed,
-                                      ),
-                                      Text(
-                                        story.date.dateTimeInDayFormat
-                                            .toUpperCase(),
-                                        style: $styles.text.caption.copyWith(
-                                          color: Colors.grey,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(height: $styles.insets.xs),
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                        left: BorderSide(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          width: 0.5,
-                                        ),
-                                      ),
-                                    ),
-                                    child: (story.photos.isNotEmpty)
-                                        ? Image.file(
-                                            story.photos.first.file!,
-                                            fit: BoxFit.cover,
-                                          )
-                                        : TitleAndDescriptionWidget(
-                                            story: story,
-                                          ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
                       );
                     },
                   ),
@@ -111,13 +54,96 @@ class _MonthPageState extends ConsumerState<MonthPage> {
     return AppBar(
       title: Text(
         DateTime(
-          widget.monthYear.year,
-          widget.monthYear.month,
+          monthYear.year,
+          monthYear.month,
         ).dateTimeInMonthYearString,
         style: $styles.text.bodyBold,
       ),
       centerTitle: true,
       actions: const [Icon(Icons.sort)],
+    );
+  }
+}
+
+class _StoryItem extends StatelessWidget {
+  final StoryModel story;
+  final double maxHeight;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
+
+  const _StoryItem({
+    Key? key,
+    required this.story,
+    required this.maxHeight,
+    required this.onTap,
+    required this.onLongPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPress: onLongPress,
+      onTap: onTap,
+      child: StyledCard(
+        customPadding: true,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: maxHeight),
+          child: Row(
+            children: [
+              _DateColumn(story: story),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        color: Colors.grey.withOpacity(0.5),
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: (story.photos.isNotEmpty)
+                      ? Image.file(
+                          story.photos.first.file!,
+                          fit: BoxFit.cover,
+                        )
+                      : TitleAndDescriptionWidget(story: story),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DateColumn extends StatelessWidget {
+  final StoryModel story;
+
+  const _DateColumn({Key? key, required this.story}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all($styles.insets.lg),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: $styles.insets.xs),
+          Text(
+            story.date.day.toString(),
+            style: $styles.text.h2Condensed,
+          ),
+          Text(
+            story.date.dateTimeInDayFormat.toUpperCase(),
+            style: $styles.text.caption.copyWith(
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: $styles.insets.xs),
+        ],
+      ),
     );
   }
 }
